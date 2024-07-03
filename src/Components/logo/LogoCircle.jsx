3b50190ead2989c5e2780273aca7b6e4595f7068
yuyingtao <yuyingtao@agiclass.ai>
2024-07-03 19:58:05 +0800

    feat: 修改 logo

--- src/Components/logo/LogoCircle.jsx
+++ src/Components/logo/LogoCircle.jsx
@@ -2,7 +2,7 @@ import { Avatar } from 'antd';
 import { GithubOutlined } from '@ant-design/icons';
 
 export const LogoCircle = ({ size = 64, style={} }) => {
-  return <Avatar size={size} icon={<GithubOutlined />} style={style} />;
+  return <Avatar size={size} src={require('@Assets/logos/logo64.png')} style={style} />;
 }
 
 export default LogoCircle;
