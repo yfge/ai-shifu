82a7c13782853981dad0d41cb4c2e9d8cda8bec4
Kenrick-Zhou <Kenrick.Zhou@gmail.com>
2024-08-14 20:48:23 +0800

    fix: Bug with no variable values in system roles on the page `Chapter Debugger`

--- src/cook/pages/1_Chapter_Debugger.py
+++ src/cook/pages/1_Chapter_Debugger.py
@@ -56,6 +56,9 @@ if 'has_started' not in st.session_state:
 if 'miss_vars' not in st.session_state:
     st.session_state.miss_vars = False
 
+if 'system_miss_vars' not in st.session_state:
+    st.session_state.system_miss_vars = False
+
 # ======================================================
 
 # ==================== Sidebar ====================
@@ -115,7 +118,8 @@ if not st.session_state.has_started:
                 # 加载剧本及系统角色
                 if 'script_list' in st.session_state:
                     del st.session_state['script_list']  # clear before load
-                load_scripts_and_system_role(st.session_state.lark_app_token, st.session_state.lark_table_id)
+                # load_scripts_and_system_role(st.session_state.lark_app_token, st.session_state.lark_table_id)
+                system_role_script = load_scripts(st.session_state.lark_app_token, st.session_state.lark_table_id)
 
             with (col3):
                 # st.session_state.select_progress = st.number_input('开始位置:', value=2, min_value=1, step=1)
@@ -150,187 +154,226 @@ else:
             st.write('')
         st.stop()
 
-    # 根据当前进度ID，获取对应的剧本
-    script: Script = st.session_state.script_list[st.session_state.progress]
-    logging.debug(f'当前剧本：\n{script}')
-    # if st.session_state.DEV_MODE:
-    #     show_current_script(script)
 
-    needed_vars = extract_variables(script.template)
-    # st.session_state
-    if needed_vars:
-        logging.debug('=== need vars')
-        missing_vars = [var for var in needed_vars if var not in st.session_state]
+    if 'system_role_script' in st.session_state and 'system_role' not in st.session_state:
+        system_needed_vars = extract_variables(st.session_state.system_role_script.template)
+        if system_needed_vars:
+            system_miss_vars = [var for var in system_needed_vars if var not in st.session_state]
+            if system_miss_vars:
+                st.session_state.system_miss_vars = True
+                with st.form('sys_miss_vars'):
+                    '### 系统角色模版中需要变量'
+                    for var in system_miss_vars:
+                        val = st.text_input(f'输入 {var} 的值：')
+                        if val != '':
+                            st.session_state[var] = val
 
-        has_empty_val = False
-        for var in needed_vars:
-            if not st.session_state.get(var):
-                has_empty_val = True
-                break
+                    submitted = st.form_submit_button('提交变量继续', type='primary', use_container_width=True)
+                    if submitted:
+                        st.session_state.system_miss_vars = False
+                        # time.sleep(5)
+                        # st.rerun()
+            else:
+                st.session_state.system_miss_vars = False
 
-        if missing_vars or has_empty_val:
-            logging.debug('=== if missing_vars or has_empty_val')
-            st.session_state.miss_vars = True
+        if not st.session_state.system_miss_vars:
+            template = st.session_state.system_role_script.template
+            variables = {v: st.session_state[v] for v in
+                         st.session_state.system_role_script.template_vars} if st.session_state.system_role_script.template_vars else None
 
-            # with st.form('missing_vars'):
-            with st.expander('Now Script Template:', expanded=True):
-                st.text_area('剧本内容', script.template, disabled=True, height=300)
-            st.write(f'需要变量: **{needed_vars}**,   缺失: **{missing_vars}**')
-            with st.form('missing_vars'):
-                for var in missing_vars:
-                    val = st.text_input(f'输入 {var} 的值：')
-                    if val != '':
-                        st.session_state[var] = val
+            if variables:
+                prompt = PromptTemplate(input_variables=list(variables.keys()), template=template)
+                prompt = prompt.format(**variables)
+            else:
+                prompt = template
 
-                submitted = st.form_submit_button('提交变量继续', type='primary', use_container_width=True)
-                if submitted:
-                    st.session_state.miss_vars = False
-                    # time.sleep(5)
-                    # st.rerun()
-        else:
-            st.session_state.miss_vars = False
+            st.session_state.system_role = prompt
+            st.session_state.system_role_id = st.session_state.system_role_script.id
 
-    # 没有缺失的 vars 时才能继续：
-    if not st.session_state.miss_vars:
+    else:
 
-        # ========== 内容输出部分 ==========
-        # 如果剧本没有输出过，则进行输出
-        if script.id not in st.session_state.script_has_output:
-            full_result = None
 
-            # ===【固定剧本】：模拟流式输出
-            if script.type == ScriptType.FIXED:
-                if script.format == ScriptFormat.MARKDOWN:
-                    logging.debug('=== 打算模拟输出了')
-                    full_result = simulate_streaming(chat_box, script.template, script.template_vars)
-                elif script.format == ScriptFormat.IMAGE:
-                    chat_box.ai_say(Image(script.media_url))
-                    full_result = script.media_url
+        # 根据当前进度ID，获取对应的剧本
+        script: Script = st.session_state.script_list[st.session_state.progress]
+        logging.debug(f'当前剧本：\n{script}')
+        # if st.session_state.DEV_MODE:
+        #     show_current_script(script)
 
-            # == 【Prompt】：剧本内容提交给 LLM，获得AI回复输出
-            elif script.type == ScriptType.PROMPT:
-                full_result = streaming_from_template(
-                    chat_box, script.template,
-                    {v: st.session_state[v] for v in script.template_vars} if script.template_vars else None,
-                    model=script.custom_model, temperature=script.temperature
-                )
+        needed_vars = extract_variables(script.template)
+        # st.session_state
+        if needed_vars:
+            logging.debug('=== need vars')
+            missing_vars = [var for var in needed_vars if var not in st.session_state]
 
-            # 最后记录下已输出的剧本ID，避免重复输出
-            st.session_state.script_has_output.add(script.id)
-            logging.debug(f'script id: {script.id}, chat result: {full_result}')
+            has_empty_val = False
+            for var in needed_vars:
+                if not st.session_state.get(var):
+                    has_empty_val = True
+                    break
 
-        # ========== 处理【后续交互】 ==========
-        # === 显示 输入框
-        if script.next_action == NextAction.ShowInput:
-            # 获取用户输入
-            if user_input := st.chat_input(script.input_placeholder):
-                chat_box.user_say(user_input)  # 展示用户输入信息
+            if missing_vars or has_empty_val:
+                logging.debug('=== if missing_vars or has_empty_val')
+                st.session_state.miss_vars = True
 
-                # 通过 `检查模版` 提取变量（JSON mode）
-                is_ok = parse_vars_from_template(chat_box, script.check_template, {'input': user_input},
-                                                 parse_keys=script.parse_vars,
-                                                 model=script.custom_model, temperature=script.temperature)
+                # with st.form('missing_vars'):
+                with st.expander('Now Script Template:', expanded=True):
+                    st.text_area('剧本内容', script.template, disabled=True, height=300)
+                st.write(f'需要变量: **{needed_vars}**,   缺失: **{missing_vars}**')
+                with st.form('missing_vars'):
+                    for var in missing_vars:
+                        val = st.text_input(f'输入 {var} 的值：')
+                        if val != '':
+                            st.session_state[var] = val
 
-                # 如果正常执行，则进入下一个剧本
-                if is_ok:
-                    st.session_state.progress += 1
-                    st.rerun()
+                    submitted = st.form_submit_button('提交变量继续', type='primary', use_container_width=True)
+                    if submitted:
+                        st.session_state.miss_vars = False
+                        # time.sleep(5)
+                        # st.rerun()
+            else:
+                st.session_state.miss_vars = False
 
-        # === 显示 按钮
-        elif script.next_action == NextAction.ShowBtn:
-            with bottom():
-                if st.button(script.btn_label, type='primary', use_container_width=True):
-                    st.session_state.progress += 1
-                    st.rerun()
+        # 没有缺失的 vars 时才能继续：
+        if not st.session_state.miss_vars:
 
-        # === 显示 按钮组
-        elif script.next_action == NextAction.ShowBtnGroup:
-            with bottom():
-                btns = distribute_elements(script.btn_group_cfg['btns'], 3, 2)
-                for row in btns:
-                    st_cols = st.columns(len(row))
-                    for i, btn in enumerate(row):
-                        if st_cols[i].button(btn['label'], key=btn['value'], type='primary',
-                                             use_container_width=True):
-                            # 获取用户点击按钮的 value
-                            st.session_state[script.btn_group_cfg['var_name']] = btn['value']
-                            st.session_state.progress += 1
-                            st.rerun()
+            # ========== 内容输出部分 ==========
+            # 如果剧本没有输出过，则进行输出
+            if script.id not in st.session_state.script_has_output:
+                full_result = None
 
-        # === 跳转按钮
-        elif script.next_action == NextAction.JumpBtn:
-            if st.button(script.btn_label, type='primary', use_container_width=True):
-                # 获取需要判断的变量值
-                var_value = st.session_state.get(script.btn_jump_cfg['var_name'])
-                # == 如果是静默跳转
-                if script.btn_jump_cfg['jump_type'] == 'silent':
-                    # 找到要跳转的子剧本
-                    lark_table_id, lark_view_id = None, None
-                    for jump_rule in script.btn_jump_cfg['jump_rule']:
-                        if var_value == jump_rule['value']:
-                            lark_table_id = jump_rule['lark_table_id']
-                            lark_view_id = jump_rule['lark_view_id']
+                # ===【固定剧本】：模拟流式输出
+                if script.type == ScriptType.FIXED:
+                    if script.format == ScriptFormat.MARKDOWN:
+                        logging.debug('=== 打算模拟输出了')
+                        full_result = simulate_streaming(chat_box, script.template, script.template_vars)
+                    elif script.format == ScriptFormat.IMAGE:
+                        chat_box.ai_say(Image(script.media_url))
+                        full_result = script.media_url
 
-                    # 如果找到了则加载，否则报错
-                    if lark_table_id:
-                        sub_script_list = load_scripts_from_bitable(cfg.LARK_APP_TOKEN, lark_table_id,
-                                                                    lark_view_id)
-                        # 将子剧本插入到原剧本中
-                        st.session_state.script_list = (
-                                st.session_state.script_list[:st.session_state.progress + 1]
-                                + sub_script_list
-                                + st.session_state.script_list[st.session_state.progress + 1:]
-                        )
-                        # 更新剧本总长度
-                        st.session_state.script_list_len = len(st.session_state.script_list)
-                        # 更新剧本进度
+                # == 【Prompt】：剧本内容提交给 LLM，获得AI回复输出
+                elif script.type == ScriptType.PROMPT:
+                    full_result = streaming_from_template(
+                        chat_box, script.template,
+                        {v: st.session_state[v] for v in script.template_vars} if script.template_vars else None,
+                        model=script.custom_model, temperature=script.temperature
+                    )
+
+                # 最后记录下已输出的剧本ID，避免重复输出
+                st.session_state.script_has_output.add(script.id)
+                logging.debug(f'script id: {script.id}, chat result: {full_result}')
+
+            # ========== 处理【后续交互】 ==========
+            # === 显示 输入框
+            if script.next_action == NextAction.ShowInput:
+                # 获取用户输入
+                if user_input := st.chat_input(script.input_placeholder):
+                    chat_box.user_say(user_input)  # 展示用户输入信息
+
+                    # 通过 `检查模版` 提取变量（JSON mode）
+                    is_ok = parse_vars_from_template(chat_box, script.check_template, {'input': user_input},
+                                                     parse_keys=script.parse_vars,
+                                                     model=script.custom_model, temperature=script.temperature)
+
+                    # 如果正常执行，则进入下一个剧本
+                    if is_ok:
                         st.session_state.progress += 1
-                        # 重新运行
                         st.rerun()
 
-                    else:
-                        raise ValueError('未找到对应的子剧本')
+            # === 显示 按钮
+            elif script.next_action == NextAction.ShowBtn:
+                with bottom():
+                    if st.button(script.btn_label, type='primary', use_container_width=True):
+                        st.session_state.progress += 1
+                        st.rerun()
 
-        # === 显示 付款码
-        elif script.next_action == NextAction.ShowPayQR:
-            pass
+            # === 显示 按钮组
+            elif script.next_action == NextAction.ShowBtnGroup:
+                with bottom():
+                    btns = distribute_elements(script.btn_group_cfg['btns'], 3, 2)
+                    for row in btns:
+                        st_cols = st.columns(len(row))
+                        for i, btn in enumerate(row):
+                            if st_cols[i].button(btn['label'], key=btn['value'], type='primary',
+                                                 use_container_width=True):
+                                # 获取用户点击按钮的 value
+                                st.session_state[script.btn_group_cfg['var_name']] = btn['value']
+                                st.session_state.progress += 1
+                                st.rerun()
 
-        # === 输入 手机号
-        elif script.next_action == NextAction.InputPhoneNum:
-            # 获取用户输入
-            if user_input := st.chat_input(script.input_placeholder):
-                chat_box.user_say(user_input)  # 展示用户输入信息
+            # === 跳转按钮
+            elif script.next_action == NextAction.JumpBtn:
+                if st.button(script.btn_label, type='primary', use_container_width=True):
+                    # 获取需要判断的变量值
+                    var_value = st.session_state.get(script.btn_jump_cfg['var_name'])
+                    # == 如果是静默跳转
+                    if script.btn_jump_cfg['jump_type'] == 'silent':
+                        # 找到要跳转的子剧本
+                        lark_table_id, lark_view_id = None, None
+                        for jump_rule in script.btn_jump_cfg['jump_rule']:
+                            if var_value == jump_rule['value']:
+                                lark_table_id = jump_rule['lark_table_id']
+                                lark_view_id = jump_rule['lark_view_id']
 
-                # 暂时不做任何处理，直接下一步
-                st.info('暂时不做任何处理，直接下一步', icon="ℹ️")
-                time.sleep(1)
+                        # 如果找到了则加载，否则报错
+                        if lark_table_id:
+                            sub_script_list = load_scripts_from_bitable(cfg.LARK_APP_TOKEN, lark_table_id,
+                                                                        lark_view_id)
+                            # 将子剧本插入到原剧本中
+                            st.session_state.script_list = (
+                                    st.session_state.script_list[:st.session_state.progress + 1]
+                                    + sub_script_list
+                                    + st.session_state.script_list[st.session_state.progress + 1:]
+                            )
+                            # 更新剧本总长度
+                            st.session_state.script_list_len = len(st.session_state.script_list)
+                            # 更新剧本进度
+                            st.session_state.progress += 1
+                            # 重新运行
+                            st.rerun()
+
+                        else:
+                            raise ValueError('未找到对应的子剧本')
+
+            # === 显示 付款码
+            elif script.next_action == NextAction.ShowPayQR:
+                pass
+
+            # === 输入 手机号
+            elif script.next_action == NextAction.InputPhoneNum:
+                # 获取用户输入
+                if user_input := st.chat_input(script.input_placeholder):
+                    chat_box.user_say(user_input)  # 展示用户输入信息
+
+                    # 暂时不做任何处理，直接下一步
+                    st.info('暂时不做任何处理，直接下一步', icon="ℹ️")
+                    time.sleep(1)
+                    st.session_state.progress += 1
+                    st.rerun()
+
+            # === 输入 验证码
+            elif script.next_action == NextAction.InputVerifyCode:
+                # 获取用户输入
+                if user_input := st.chat_input(script.input_placeholder):
+                    chat_box.user_say(user_input)  # 展示用户输入信息
+
+                    # 暂时不做任何处理，直接下一步
+                    st.info('暂时不做任何处理，直接下一步', icon="ℹ️")
+                    time.sleep(1)
+                    st.session_state.progress += 1
+                    st.rerun()
+
+            else:
                 st.session_state.progress += 1
                 st.rerun()
 
-        # === 输入 验证码
-        elif script.next_action == NextAction.InputVerifyCode:
-            # 获取用户输入
-            if user_input := st.chat_input(script.input_placeholder):
-                chat_box.user_say(user_input)  # 展示用户输入信息
-
-                # 暂时不做任何处理，直接下一步
-                st.info('暂时不做任何处理，直接下一步', icon="ℹ️")
-                time.sleep(1)
-                st.session_state.progress += 1
-                st.rerun()
-
-        else:
-            st.session_state.progress += 1
-            st.rerun()
 
 
+        # st.session_state
 
-    # st.session_state
-
-    # # 开发者模式要做的事情
-    # if st.session_state.DEV_MODE:
-    #     # 加载进度控制器
-    #     load_process_controller()
+        # # 开发者模式要做的事情
+        # if st.session_state.DEV_MODE:
+        #     # 加载进度控制器
+        #     load_process_controller()
 
 
 
