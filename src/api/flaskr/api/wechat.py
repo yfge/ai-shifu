889c230ed15df07b5bb9a7cf39e490dddd2ff2ef
yfge <geyunfei@gmail.com>
2024-08-19 20:31:45 +0800

    update wechat accessken

--- src/api/flaskr/api/wechat.py
+++ src/api/flaskr/api/wechat.py
@@ -7,7 +7,7 @@ def get_wechat_access_token(app:Flask, code:str):
     app.logger.info("get_wechat_access_token")
     app_id = app.config.get("WECHAT_APP_ID")
     app_secret = app.config.get("WECHAT_APP_SECRET")
-    url = f"https://api.weixin.qq.com/sns/oauth2/access_token?appid={app_id}&secret={app_secret}&code=CODE&grant_type=authorization_code"
+    url = f"https://api.weixin.qq.com/sns/oauth2/access_token?appid={app_id}&secret={app_secret}&code={code}&grant_type=authorization_code"
     response = requests.get(url)
     if response.status_code == 200:
         app.logger.info('get_wechat_access_token:'+str(response.json()))
