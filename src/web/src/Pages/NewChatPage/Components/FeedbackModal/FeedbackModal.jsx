1e7ad9a66db956d35c6483d0d5bfbc6e3ad16a72
moemoefish <moemoefish@qq.com>
2024-08-31 01:30:18 +0800

    feat: 提交反馈功能修复

--- src/web/src/Pages/NewChatPage/Components/FeedbackModal/FeedbackModal.jsx
+++ src/web/src/Pages/NewChatPage/Components/FeedbackModal/FeedbackModal.jsx
@@ -1,15 +1,29 @@
-import { Modal, Form, Input } from 'antd';
+import { Modal, Form, Input, message } from 'antd';
 import MainButton from 'Components/MainButton.jsx';
 import { calModalWidth } from 'Utils/common.js';
 import styles from './FeedbackModal.module.scss';
 import { memo } from 'react';
+import { useCallback } from 'react';
+import { submitFeedback } from 'Api/bz.js';
 
 const FEEDBACK_MAX_LENGTH = 300;
 
 export const FeedbackModal = ({ open, onClose, inMobile = false }) => {
   const { TextArea } = Input;
+  const [form] = Form.useForm();
+  const [messageApi, contextHolder] = message.useMessage();
 
-  const onSubmitFeedback = () => {};
+  const onSubmitFeedback = useCallback(async () => {
+    try {
+      const data = await form.validateFields();
+      const { feedback } = data;
+      await submitFeedback(feedback);
+      messageApi.success({
+        content: '反馈成功，感谢你的反馈。'
+      });
+      onClose();
+    } catch {}
+  }, [form, messageApi, onClose]);
 
   return (
     <Modal
@@ -21,8 +35,8 @@ export const FeedbackModal = ({ open, onClose, inMobile = false }) => {
       onCancel={onClose}
     >
       <div className={styles.title}>反馈</div>
-      <Form className={styles.formWrapper}>
-        <Form.Item name="feedback">
+      <Form className={styles.formWrapper} form={form}>
+        <Form.Item name="feedback" rules={[{ required: true, message: '请输入反馈内容' }]}>
           <TextArea
             name="feedback"
             showCount
@@ -31,9 +45,10 @@ export const FeedbackModal = ({ open, onClose, inMobile = false }) => {
           />
         </Form.Item>
       </Form>
-      <MainButton className={styles.okBtn} width="100%">
+      <MainButton className={styles.okBtn} width="100%" onClick={onSubmitFeedback}>
         提交
       </MainButton>
+      { contextHolder }
     </Modal>
   );
 };
