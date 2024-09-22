9e620fd1880b8d2a17390cabe1b9f72fd9d5184e
yfge <geyunfei@gmail.com>
2024-09-22 12:09:23 +0800

    update funcname

--- src/api/flaskr/common/log.py
+++ src/api/flaskr/common/log.py
@@ -1,46 +1,51 @@
 import logging
 import os
-from flask import Flask,g ,request
+from flask import Flask, request
 import uuid
 from logging.handlers import TimedRotatingFileHandler
 import threading
 import socket
 from datetime import datetime
 import pytz
-thread_local = threading.local() 
+
+thread_local = threading.local()
+
+
 class RequestFormatter(logging.Formatter):
     def formatTime(self, record, datefmt=None):
-        # 创建时区信息
-        bj_time = pytz.timezone('Asia/Shanghai')
+        # create time zone info
+        bj_time = pytz.timezone("Asia/Shanghai")
         # 转换 record.created（一个浮点数时间戳）到北京时间
         ct = datetime.fromtimestamp(record.created, bj_time)
         if datefmt:
             s = ct.strftime(datefmt)
         else:
             try:
-                s = ct.isoformat(timespec='milliseconds')
+                s = ct.isoformat(timespec="milliseconds")
             except TypeError:
                 s = ct.isoformat()
         return s
+
     def format(self, record):
         try:
-            record.url = getattr(thread_local, 'url', 'No_URL')
-            record.request_id = getattr(thread_local, 'request_id', 'No_Request_ID')
-            record.client_ip = getattr(thread_local, 'client_ip', 'No_Client_IP')
+            record.url = getattr(thread_local, "url", "No_URL")
+            record.request_id = getattr(thread_local, "request_id", "No_Request_ID")
+            record.client_ip = getattr(thread_local, "client_ip", "No_Client_IP")
         except RuntimeError:
             record.url = "No_URL"
             record.request_id = "No_Request_ID"
             record.client_ip = "No_Client_IP"
         return super().format(record)
 
-def init_log(app:Flask)->Flask:
+
+def init_log(app: Flask) -> Flask:
     @app.before_request
     def setup_logging():
         thread_local.request_id = uuid.uuid4().hex
         thread_local.url = request.path
-            # 尝试从 X-Forwarded-For 头部获取 IP 地址
-        if 'X-Forwarded-For' in request.headers:
-            user_ip = request.headers['X-Forwarded-For'].split(',')[0].strip()
+        # 尝试从 X-Forwarded-For 头部获取 IP 地址
+        if "X-Forwarded-For" in request.headers:
+            user_ip = request.headers["X-Forwarded-For"].split(",")[0].strip()
         else:
             # 如果没有 X-Forwarded-For 头部，就使用 remote_addr
             user_ip = request.remote_addr
@@ -48,23 +53,30 @@ def init_log(app:Flask)->Flask:
         thread_local.client_ip = user_ip
 
     host_name = socket.gethostname()
-    
-    log_format = '%(asctime)s [%(levelname)s] ai-shifu.com/ai-sifu '+host_name+' %(client_ip)s %(url)s %(request_id)s %(message)s'
+
+    # log_format = '%(asctime)s [%(levelname)s] ai-shifu.com/ai-sifu '+host_name+' %(client_ip)s %(url)s %(request_id)s %(message)s'
+    log_format = (
+        "%(asctime)s [%(levelname)s] ai-shifu.com/ai-sifu "
+        + host_name
+        + " %(client_ip)s %(url)s %(request_id)s %(funcName)s %(message)s"
+    )
     formatter = RequestFormatter(log_format)
-    
-    log_file = app.config.get('LOGGING_PATH', 'logs/ai-sifu.log')
+
+    log_file = app.config.get("LOGGING_PATH", "logs/ai-sifu.log")
     # 如果目录不存在，创建目录
     log_dir = os.path.dirname(log_file)
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
-    # 按天切割日志 
-    file_handler = TimedRotatingFileHandler(app.config['LOGGING_PATH'], when='midnight', backupCount=7)
+    # 按天切割日志
+    file_handler = TimedRotatingFileHandler(
+        app.config["LOGGING_PATH"], when="midnight", backupCount=7
+    )
     file_handler.setFormatter(formatter)
     # 控制台日志处理器
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
-    if 'gunicorn' in os.getenv('SERVER_SOFTWARE', ''):
-        gunicorn_logger = logging.getLogger('gunicorn.info')
+    if "gunicorn" in os.getenv("SERVER_SOFTWARE", ""):
+        gunicorn_logger = logging.getLogger("gunicorn.info")
         if gunicorn_logger.handlers:
             for handler in gunicorn_logger.handlers:
                 handler.setFormatter(formatter)
@@ -81,5 +93,5 @@ def init_log(app:Flask)->Flask:
     app.logger.setLevel(logging.INFO)
     app.logger.propagate = False  # 停止向上传播
     app.logger.setLevel(logging.INFO)
-    app.logger.error('Logging setup complete')
-    return app 
+    app.logger.error("Logging setup complete")
+    return app
