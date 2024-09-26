9a02523ea42b64e069673752a95386203c9d1cf9
yfge <geyunfei@gmail.com>
2024-09-26 15:07:43 +0800

    add api info

--- src/api/flaskr/route/dicts.py
+++ src/api/flaskr/route/dicts.py
@@ -1,22 +1,31 @@
-from flask import Flask,request
-import json
+from flask import Flask
 
 from flaskr.api.llm import get_current_models
-from flaskr.service.common.models import raise_param_error
-from .common import make_common_response,bypass_token_validation
+from .common import make_common_response, bypass_token_validation
 from ..service.common.dicts import get_all_dicts
 
 
-def register_dict_handler(app:Flask,path_prefix:str)->Flask:
-    @app.route(path_prefix+'/dicts', methods=['GET'])
+def register_dict_handler(app: Flask, path_prefix: str) -> Flask:
+    @app.route(path_prefix + "/dicts", methods=["GET"])
     @bypass_token_validation
     def get_dicts():
-        return make_common_response(get_all_dicts(app)) 
-    
-    @app.route(path_prefix+"/models",methods=['GET'])
+        """
+        获取所有字典
+        ---
+        tags:
+          - 字典
+        """
+        return make_common_response(get_all_dicts(app))
+
+    @app.route(path_prefix + "/models", methods=["GET"])
     @bypass_token_validation
     def get_models():
+        """
+        获取所有模型
+        ---
+        tags:
+          - 字典
+        """
         return make_common_response(get_current_models(app))
-    return app
 
-   
+    return app
