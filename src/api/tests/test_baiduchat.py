095811de9561b34b6c0af62319fdb74e12789e72
yfge <geyunfei@gmail.com>
2024-09-27 12:46:25 +0800

    fix online error

--- src/api/tests/test_baiduchat.py
+++ src/api/tests/test_baiduchat.py
@@ -1,21 +1,14 @@
-import requests
-from flask import Flask 
-import pytest
-# from .common import create_app
-
-
-  
-
-
-# from ..dao import redis_client
-
 def test_get_access_token(app):
     from flaskr.api.llm.ernie import get_access_token
+
     access_token = get_access_token()
     app.logger.info(access_token)
-    assert get_access_token() != ''
-    
+    assert get_access_token() != ""
+
 
 def test_chat(app):
     from flaskr.api.llm.ernie import get_ernie_response
-    get_ernie_response(app,"ERNIE-Speed-8K","你好")
+
+    resp = get_ernie_response(app, "ERNIE-4.0-8K", "你好,最近有什么新闻")
+    for r in resp:
+        app.logger.info(r.result)
