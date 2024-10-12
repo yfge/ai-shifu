e108f3b198517150b0070a65abd1848fbbb594aa
yfge <geyunfei@gmail.com>
2024-10-12 16:54:33 +0800

    feat: login ui support

--- src/api/flaskr/service/study/input/handle_input_login.py
+++ src/api/flaskr/service/study/input/handle_input_login.py
@@ -4,12 +4,12 @@ from flaskr.service.study.input_funcs import BreakException
 from flaskr.service.user.models import User
 from flaskr.service.lesson.models import AILessonScript
 from flaskr.service.order.models import AICourseLessonAttend
-from flaskr.service.study.const import INPUT_TYPE_LOGIN
+from flaskr.service.study.const import INPUT_TYPE_REQUIRE_LOGIN
 from flaskr.service.study.plugin import register_input_handler
 from flaskr.service.study.utils import make_script_dto
 
 
-@register_input_handler(input_type=INPUT_TYPE_LOGIN)
+@register_input_handler(input_type=INPUT_TYPE_REQUIRE_LOGIN)
 def handle_input_login(
     app: Flask,
     user_id: str,
