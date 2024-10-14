cd1693544a696a898066b5c3f80ec51b4f2dab0a
yfge <geyunfei@gmail.com>
2024-10-14 19:54:38 +0800

    fix: input

--- src/web/src/Pages/NewChatPage/Components/ChatUi/ChatInput/ChatInputText.jsx
+++ src/web/src/Pages/NewChatPage/Components/ChatUi/ChatInput/ChatInputText.jsx
@@ -48,7 +48,7 @@ export const ChatInputText = ({ onClick, type, disabled = false,props={} }) => {
       <div className={styles.inputForm}>
         <div className={styles.inputWrapper}>
           <Input
-            autoSize={{ minRows: 1, maxRows: 5 }} // 自动显示多行
+            autoSize={{ minRows: 1, maxRows: 5 }}
             type="text"
             value={input}
             onChange={(v) => {
