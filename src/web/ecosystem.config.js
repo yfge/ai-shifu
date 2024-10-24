ac6bd8d64cc7ee0f5989c3c83e92ab49fd2060ea
拐爷&&老拐瘦 <geyunfei@gmail.com>
2024-10-24 13:02:55 +0800

    feat: add pm2

---
+++ src/web/ecosystem.config.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+console.log('all env', process.env);
+module.exports = {
+  apps: [
+    {
+      name: 'ai-shifu-web',
+      script: 'serve -s build -l '+(process.env.PORT || '5800'),
+      env: {
+        PORT: process.env.PORT,
+        REACT_APP_BASEURL: process.env.REACT_APP_BASEURL,
+        REACT_APP_COURSE_ID: process.env.REACT_APP_COURSE_ID,
+        REACT_APP_APP_ID: process.env.REACT_APP_APP_ID,
+        REACT_APP_UMAMI_WEBSITE_ID: process.env.REACT_APP_UMAMI_WEBSITE_ID,
+        REACT_APP_UMAMI_SCRIPT_SRC: process.env.REACT_APP_UMAMI_SCRIPT_SRC,
+      },
+    }
+  ]
+};
