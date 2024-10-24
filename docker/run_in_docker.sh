ad7a843c3314b13d4798ebefa7b212ebfac479a0
拐爷&&老拐瘦 <geyunfei@gmail.com>
2024-10-24 09:50:57 +0800

    ci: set run in docker image

--- docker/run_in_docker.sh
+++ docker/run_in_docker.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-
-if [ ! -f ".env" ]; then
-    cp .env.example .env
-fi
-
-cd ../src/api
-docker build ./ -t ai-shifu-api
-cd ../web
-docker build ./ -t ai-shifu-web
-cd ..
-cd ..
-cd docker
 docker compose up
