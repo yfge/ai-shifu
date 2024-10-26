177d6ebd4597ff313a929cc74e945427072e39ac
拐爷&&老拐瘦 <geyunfei@gmail.com>
2024-10-27 02:25:36 +0800

    chore: add -f to remove local docker image

--- docker/run_in_docker.sh
+++ docker/run_in_docker.sh
@@ -5,5 +5,5 @@ if [ ! -f ".env" ]; then
 fi
 
 cd docker
-docker images | grep 'aishifu'  | awk -F ' ' '{print $3}' | xargs -I {} docker image rm {}
+docker images | grep 'aishifu'  | awk -F ' ' '{print $3}' | xargs -I {} docker image rm -f {}
 docker compose up
