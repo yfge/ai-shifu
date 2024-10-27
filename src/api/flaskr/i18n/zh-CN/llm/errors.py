ba90bdebf9001b699786d0cb06d2b4bcae1b26d5
拐爷&&老拐瘦 <geyf@vip.qq.com>
2024-10-27 13:26:21 +0800

    Update src/api/flaskr/i18n/zh-CN/llm/errors.py
    Co-authored-by: Sun Zhigang <sunner@gmail.com>
    Signed-off-by: 拐爷&&老拐瘦 <geyf@vip.qq.com>

--- src/api/flaskr/i18n/zh-CN/llm/errors.py
+++ src/api/flaskr/i18n/zh-CN/llm/errors.py
@@ -1,3 +1,3 @@
-NO_DEFAULT_LLM_CONFIGURED = "没有默认的LLM配置"
-SPECIFIC_LLM_NOT_CONFIGURED = "模型 {} 没有配置,请检查配置项:{}"
-NO_LLM_CONFIGURED = "没有配置LLM"
+NO_DEFAULT_LLM = "没有配置默认的大模型"
+SPECIFIED_LLM_NOT_CONFIGURED = "模型 {} 没有配置，请检查 .env 中的变量：{}"
+NO_LLM_CONFIGURED = "没有配置大模型"
