0bbd2683115a7843744928cde3bbcc745f83deab
拐爷&&老拐瘦 <geyunfei@gmail.com>
2024-10-29 19:08:54 +0800

    fix: modify the mobie when the checkcode error (#150)

--- src/api/flaskr/service/study/ui/input_checkcode.py
+++ src/api/flaskr/service/study/ui/input_checkcode.py
@@ -7,7 +7,7 @@ from flaskr.service.lesson.models import AILessonScript
 from flaskr.service.order.models import AICourseLessonAttend
 from flaskr.service.study.const import INPUT_TYPE_CHECKCODE
 from flaskr.service.study.plugin import register_ui_handler
-from flaskr.service.study.utils import make_script_dto
+from flaskr.service.study.utils import make_script_dto, check_phone_number
 from flaskr.service.user.common import send_sms_code_without_check
 
 
@@ -22,9 +22,17 @@ def handle_input_checkcode(
     trace_args,
 ):
     try:
-        expires = send_sms_code_without_check(app, user_id, input)
-        expires["content"] = script_info.script_ui_content
-        yield make_script_dto(INPUT_TYPE_CHECKCODE, expires, script_info.script_id)
+        if check_phone_number(app, user_id, input):
+            expires = send_sms_code_without_check(app, user_id, input)
+            expires["content"] = script_info.script_ui_content
+            yield make_script_dto(INPUT_TYPE_CHECKCODE, expires, script_info.script_id)
+        else:
+            app.logger.info("handle_input_checkcode input is not phone number:" + input)
+            yield make_script_dto(
+                INPUT_TYPE_CHECKCODE,
+                script_info.script_ui_content,
+                script_info.script_id,
+            )
     except AppException as e:
         for i in e.message:
             yield make_script_dto("text", i, script_info.script_id)
