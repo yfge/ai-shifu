78f688dd5f79148065836ad1e5b59611db64c257
拐爷&&老拐瘦 <geyunfei@gmail.com>
2024-10-30 16:58:17 +0800

    fix: web build with debug (#160)

--- src/web/craco.config.js
+++ src/web/craco.config.js
@@ -1,35 +1,38 @@
-const cracoAlias = require("craco-alias");
+const cracoAlias = require('craco-alias');
 
 module.exports = {
   plugins: [
     {
       plugin: cracoAlias,
       options: {
-        baseUrl: "./src",
-        source: "jsconfig",
-      }
-    }
+        baseUrl: './src',
+        source: 'jsconfig',
+      },
+    },
   ],
   devServer: {
     client: {
       overlay: {
         errors: true,
         warnings: false,
-        runtimeErrors: false, // 停止运行时错误的 overlay 提示
-      }
+        runtimeErrors: false,
+      },
     },
   },
   webpack: {
     configure: (webpackConfig) => {
-      // 查找 Webpack rules 的 index
-      const oneOfRuleIndex = webpackConfig.module.rules.findIndex(rule => rule.oneOf);
+      const oneOfRuleIndex = webpackConfig.module.rules.findIndex(
+        (rule) => rule.oneOf
+      );
 
       if (oneOfRuleIndex >= 0) {
-        // 为 Markdown 文件添加 raw-loader
         webpackConfig.module.rules[oneOfRuleIndex].oneOf.unshift({
           test: /\.md$/,
           use: 'raw-loader',
         });
+      }
+      if (process.env.NODE_ENV === 'production') {
+        webpackConfig.devtool = false;
       }
 
       return webpackConfig;
