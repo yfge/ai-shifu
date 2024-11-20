784effc62ce3783429187206084970ddec7e42b8
拐爷&&老拐瘦 <geyunfei@gmail.com>
2024-11-20 09:51:58 +0800

    feat: add feishu log handler (#198)
    fix: remove setup log error
    
    chore: remove unuseful code

--- src/api/flaskr/common/log.py
+++ src/api/flaskr/common/log.py
@@ -8,6 +8,7 @@ import socket
 from datetime import datetime
 import pytz
 import colorlog
+import requests
 
 thread_local = threading.local()
 
@@ -43,34 +44,46 @@ class RequestFormatter(logging.Formatter):
         return super().format(record)
 
 
+class FeishuLogHandler(logging.Handler):
+    def __init__(self, webhook_url):
+        super().__init__(level=logging.ERROR)
+        self.webhook_url = webhook_url
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        payload = {
+            "msg_type": "text",
+            "content": {"text": f"师傅出错啦！\n{log_entry}\n"},
+        }
+        try:
+            response = requests.post(self.webhook_url, json=payload)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to send log to Feishu: {e}")
+
+
 def init_log(app: Flask) -> Flask:
     @app.before_request
     def setup_logging():
-
-        # request_id = getattr(thread_local, "X-Request-ID", uuid.uuid4().hex)
         request_id = request.headers.get("X-Request-ID", uuid.uuid4().hex)
         mode = request.headers.get("X-API-MODE", "api")
         thread_local.mode = mode
         thread_local.request_id = request_id
         thread_local.url = request.path
-        # try to get user ip from X-Forwarded-For header
         if "X-Forwarded-For" in request.headers:
             user_ip = request.headers["X-Forwarded-For"].split(",")[0].strip()
         else:
-            # if no X-Forwarded-For header, use remote_addr
             user_ip = request.remote_addr
         request.client_ip = user_ip
         thread_local.client_ip = user_ip
 
     host_name = socket.gethostname()
-
     log_format = (
         "%(asctime)s [%(levelname)s] ai-shifu.com/ai-sifu "
         + host_name
         + " %(client_ip)s %(url)s %(request_id)s %(funcName)s %(process)d %(message)s"
     )
     formatter = RequestFormatter(log_format)
-
     # color log format
     color_log_format = (
         "%(log_color)s%(asctime)s [%(levelname)s] ai-shifu.com/ai-sifu "
@@ -113,10 +126,16 @@ def init_log(app: Flask) -> Flask:
         app.logger.handlers = []
         app.logger.addHandler(file_handler)
         app.logger.addHandler(console_handler)
+    feishu_webhook_url = app.config.get("FEISHU_LOG_WEBHOOK_URL", None)
+    if feishu_webhook_url:
+        app.logger.info("Feishu enabled.")
+        feishu_handler = FeishuLogHandler(feishu_webhook_url)
+        feishu_handler.setFormatter(formatter)
+        app.logger.addHandler(feishu_handler)
+    else:
+        app.logger.info("Feishu disabled.")
     app.logger.setLevel(logging.INFO)
     app.logger.propagate = False
-    app.logger.setLevel(logging.INFO)
-    app.logger.error("Logging setup complete")
     return app
 
 
