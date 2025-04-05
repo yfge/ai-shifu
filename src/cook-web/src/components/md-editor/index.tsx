b8060cc141bfdb491d293bcc7cda74130c7887f1
hongyin163 <hongyin163@163.com>
2025-04-05 12:13:10 +0800

    fix eslint error

--- src/cook-web/src/components/md-editor/index.tsx
+++ src/cook-web/src/components/md-editor/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react'
-import { LeafDirective } from 'mdast-util-directive'
+// import { LeafDirective } from 'mdast-util-directive'
 import {
     diffSourcePlugin,
     markdownShortcutPlugin,
@@ -94,44 +94,44 @@ export async function expressImageUploadHandler(image: File) {
     return json.url
 }
 
-interface YoutubeDirectiveNode extends LeafDirective {
-    name: 'youtube'
-    attributes: { id: string }
-}
+// interface YoutubeDirectiveNode extends LeafDirective {
+//     name: 'youtube'
+//     attributes: { id: string }
+// }
 
-export const YoutubeDirectiveDescriptor: DirectiveDescriptor<YoutubeDirectiveNode> = {
-    name: 'youtube',
-    type: 'leafDirective',
-    testNode(node) {
-        return node.name === 'youtube'
-    },
-    attributes: ['id'],
-    hasChildren: false,
-    Editor: ({ mdastNode, lexicalNode, parentEditor }) => {
-        return (
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-                <button
-                    onClick={() => {
-                        parentEditor.update(() => {
-                            lexicalNode.selectNext()
-                            lexicalNode.remove()
-                        })
-                    }}
-                >
-                    delete
-                </button>
-                <iframe
-                    width="560"
-                    height="315"
-                    src={`https://www.youtube.com/embed/${mdastNode.attributes.id}`}
-                    title="YouTube video player"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                ></iframe>
-            </div>
-        )
-    }
-}
+// export const YoutubeDirectiveDescriptor: DirectiveDescriptor<YoutubeDirectiveNode> = {
+//     name: 'youtube',
+//     type: 'leafDirective',
+//     testNode(node) {
+//         return node.name === 'youtube'
+//     },
+//     attributes: ['id'],
+//     hasChildren: false,
+//     Editor: ({ mdastNode, lexicalNode, parentEditor }) => {
+//         return (
+//             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+//                 <button
+//                     onClick={() => {
+//                         parentEditor.update(() => {
+//                             lexicalNode.selectNext()
+//                             lexicalNode.remove()
+//                         })
+//                     }}
+//                 >
+//                     delete
+//                 </button>
+//                 <iframe
+//                     width="560"
+//                     height="315"
+//                     src={`https://www.youtube.com/embed/${mdastNode.attributes.id}`}
+//                     title="YouTube video player"
+//                     frameBorder="0"
+//                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+//                 ></iframe>
+//             </div>
+//         )
+//     }
+// }
 
 export const ALL_PLUGINS = [
     toolbarPlugin({
