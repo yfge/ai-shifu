d36f5e65414e73835d5445c1ad931c581c257c29
aichy <work@aichy.com>
2025-04-22 23:05:06 +0800

    add AILesson equals func (#377)
    * add mail login
    
    * verify email code
    
    * user set password
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update swagger
    
    * add  AILesson equals func
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: aichy126 <ai@aichy.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: 拐爷&&老拐瘦 <geyunfei@gmail.com>

--- src/api/flaskr/service/lesson/models.py
+++ src/api/flaskr/service/lesson/models.py
@@ -216,6 +216,23 @@ class AILesson(db.Model):
     def is_final(self):
         return len(self.lesson_no) > 2
 
+    def __eq__(self, other):
+        if not isinstance(other, AILesson):
+            return NotImplemented
+        return (
+            self.lesson_id == other.lesson_id
+            and self.lesson_no == other.lesson_no
+            and self.lesson_name == other.lesson_name
+            and self.lesson_desc == other.lesson_desc
+            and self.course_id == other.course_id
+            and self.created_user_id == other.created_user_id
+            and self.updated_user_id == other.updated_user_id
+            and self.status == other.status
+            and self.lesson_index == other.lesson_index
+            and self.lesson_type == other.lesson_type
+            and self.updated_at == other.updated_at
+        )
+
     def clone(self):
         return AILesson(
             course_id=self.course_id,
