ce58cc13540cc6dba51830270901652378dbe478
拐爷&&老拐瘦 <geyunfei@gmail.com>
2025-05-09 00:47:09 +0800

    feat: update i18n conf

--- src/cook-web/src/components/add-block/index.tsx
+++ src/cook-web/src/components/add-block/index.tsx
@@ -6,6 +6,7 @@ import { useContentTypes } from '../render-block'
 import { BlockType } from '@/types/scenario'
 
 export default function AddBlock({ onAdd }: { onAdd: (type: BlockType) => void }) {
+    const contentTypes = useContentTypes()
     return (
         <DropdownMenu>
             <DropdownMenuTrigger>
@@ -15,7 +16,7 @@ export default function AddBlock({ onAdd }: { onAdd: (type: BlockType) => void }
             </DropdownMenuTrigger>
             <DropdownMenuContent align='start' side='bottom' alignOffset={-5}>
                 {
-                    useContentTypes().map((item) => {
+                    contentTypes.map((item) => {
                         return (
                             <DropdownMenuItem key={item.type} onClick={() => onAdd(item.type as BlockType)}>
                                 <span className='text-sm'>{item.name}</span>
