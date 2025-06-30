96b80d77bb0648659fba95e97e5d8cfff803cb1f
拐爷&&老拐瘦 <geyunfei@gmail.com>
2025-06-30 21:30:29 +0800

    feat: enhance import user functionality by adding nickname parameter (#599)

--- src/api/flaskr/command/import_user.py
+++ src/api/flaskr/command/import_user.py
@@ -8,7 +8,9 @@ import uuid
 from flaskr.dao import db
 
 
-def import_user(app: Flask, mobile, course_id, discount_code="web"):
+def import_user(
+    app: Flask, mobile, course_id, discount_code="web", user_nick_name=None
+):
     """Import user and enable course"""
     app.logger.info(f"import_user: {mobile}, {course_id}")
     with app.app_context():
@@ -22,6 +24,8 @@ def import_user(app: Flask, mobile, course_id, discount_code="web"):
             user = User.query.filter_by(user_id=user.userInfo.user_id).first()
             user.mobile = mobile
             user.user_state = USER_STATE_REGISTERED
+            if user_nick_name:
+                user.name = user_nick_name
             db.session.commit()
             user_id = user.user_id
         else:
