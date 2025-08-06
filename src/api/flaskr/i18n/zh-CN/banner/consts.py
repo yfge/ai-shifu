60340c080fd63a7018a58fe73292319de5ef0a95
拐爷&&老拐瘦 <geyunfei@gmail.com>
2025-08-06 10:44:50 +0800

    fix

--- src/api/flaskr/i18n/zh-CN/banner/consts.py
+++ src/api/flaskr/i18n/zh-CN/banner/consts.py
@@ -1,11 +1,9 @@
-BANNER_TITLE = "Unlock All"
-BANNER_POP_UP_TITLE = "Unlock All Content Anytime"
-BANNER_POP_UP_CONTENT = "Gain unlimited access and enjoy stress-free learning"
-BANNER_POP_UP_CONFIRM_TEXT = "Unlock Now!"
-BANNER_POP_UP_CANCEL_TEXT = "Keep Exploring"
+BANNER_TITLE = "解锁全部"
+BANNER_POP_UP_TITLE = "在此随时解锁全部内容"
+BANNER_POP_UP_CONTENT = "立即获得无限追问等诸多权益，让学习从容无忧"
+BANNER_POP_UP_CONFIRM_TEXT = "立即解锁"
+BANNER_POP_UP_CANCEL_TEXT = "再学学看"
 
 
-LESSON_BANNER_TITLE = "Unlock All Content"
-LESSON_BANNER_CONTENT = (
-    "{chapter_count} chapters and {lesson_count} lessons, customized for you"
-)
+LESSON_BANNER_TITLE = "立即解锁 全部内容"
+LESSON_BANNER_CONTENT = "共 {chapter_count} 章 {lesson_count} 节，为您专属定制"
