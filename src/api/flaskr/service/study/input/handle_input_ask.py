86c94fa7fd757aec8b3dfe1955238a2787d88640
yfge <geyunfei@gmail.com>
2025-08-13 14:01:42 +0800

    chore: add fmt try-except

--- src/api/flaskr/service/study/input/handle_input_ask.py
+++ src/api/flaskr/service/study/input/handle_input_ask.py
@@ -59,7 +59,11 @@ def _handle_input_ask(
     context = RunScriptContext.get_current_context(app)
     app.logger.info("follow_up_info:{}".format(follow_up_info.__json__()))
 
-    ask_message_limit = int(app.config.get("ASK_MESSAGE_LIMIT", 10))
+    raw_ask_message_limit = app.config.get("ASK_MESSAGE_LIMIT", 10)
+    try:
+        ask_message_limit = int(raw_ask_message_limit)
+    except ValueError:
+        ask_message_limit = 10
 
     # Query historical conversation records, ordered by time
     history_scripts = (
