09111bb8a56771d37990bd62ab4bdc48d5b07a2f
yfge <geyunfei@gmail.com>
2025-10-15 15:49:06 +0800

    fix:reload

--- src/api/flaskr/service/learn/context_v2.py
+++ src/api/flaskr/service/learn/context_v2.py
@@ -1126,8 +1126,8 @@ class RunScriptContextV2:
                     LearnGeneratedBlock.outline_item_bid
                     == generated_block.outline_item_bid,
                     LearnGeneratedBlock.user_bid == self._user_info.user_id,
-                    LearnGeneratedBlock.id > generated_block.id,
-                    LearnGeneratedBlock.position > generated_block.position,
+                    LearnGeneratedBlock.id >= generated_block.id,
+                    LearnGeneratedBlock.position >= generated_block.position,
                 ).update(
                     {
                         LearnGeneratedBlock.status: 0,
