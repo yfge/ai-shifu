9a839ab200e9400c7a1d0e7ee1018944c52d6996
yfge <geyunfei@gmail.com>
2025-10-20 00:09:09 +0800

    refactor: move banner texts to shared json

--- src/api/flaskr/i18n/zh-CN/banner/consts.py
+++
@@ -1,9 +0,0 @@
-BANNER_TITLE = "解锁全部"
-BANNER_POP_UP_TITLE = "在此随时解锁全部内容"
-BANNER_POP_UP_CONTENT = "立即获得无限追问等诸多权益，让学习从容无忧"
-BANNER_POP_UP_CONFIRM_TEXT = "立即解锁"
-BANNER_POP_UP_CANCEL_TEXT = "再学学看"
-
-
-LESSON_BANNER_TITLE = "立即解锁 全部内容"
-LESSON_BANNER_CONTENT = "共 {chapter_count} 章 {lesson_count} 节，为您专属定制"
