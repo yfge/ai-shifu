74c44be33e47139eb0746044768ab1e72cc62572
yfge <geyunfei@gmail.com>
2025-10-19 23:23:51 +0800

    refactor: move order error strings to json

--- src/api/flaskr/i18n/en-US/order/errors.py
+++
@@ -1,8 +0,0 @@
-ORDER_NOT_FOUND = "Order Not Found"
-ORDER_ALREADY_EXISTS = "Order Already Exists"
-ORDER_STATUS_ERROR = "Order Status Error"
-ORDER_PAY_ERROR = "订单支付失败"
-ORDER_REFUND_ERROR = "Order Refund Error"
-ORDER_PAY_EXPIRED = "Order Pay Expired"
-ORDER_PAY_NOT_FOUND = "Order Pay Not Found"
-ORDER_HAS_PAID = "Order Has Paid"
